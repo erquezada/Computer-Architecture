6e6396d6f7b3b659d8bc2518e5c38cb4fdf70457
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2024-12-27 14:28:17 -0700

    Update exercise1.c

--- C-Programming-Language/exercise1.c
+++ C-Programming-Language/exercise1.c
@@ -1,6 +1,8 @@
-#include "stdio.h"
+#include <stdio.h>
 
-int main () {
-  printf("Hello, World");
-  return 0;
+int main() {
+    printf("hello, ");
+    printf("world");
+    printf("\n");
+    return 0;
 }
