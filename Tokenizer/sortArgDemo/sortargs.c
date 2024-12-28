db16babcb7eae10bf225fc06433eccaafeea0073
Eric Quezada <81540741+erquezada@users.noreply.github.com>
2024-12-28 11:40:30 -0700

    Update sortargs.c

--- Tokenizer/sortArgDemo/sortargs.c
+++ Tokenizer/sortArgDemo/sortargs.c
@@ -2,13 +2,10 @@
   
 int main(int argc, char **argv)	/* prints arguments in order */
 {
-  printf("the program name is <%s>\n", *(argv+0)); // I don't get this.
-  for (char **p = &argv[1]; *p; p++) // why is p a double pointer?
+  printf("the program name is <%s>\n", *(argv+0));   
+  for (char **p = &argv[1]; *p; p++) 
     bstInsert(&bstRoot, *p);
-  // ^^ inserts a new word into the tree
   bstPrint(bstRoot);
-  // ^^ just prints it in order
   bstClear(&bstRoot);
-  // IF YOU DON'T FREE THE MEMORY
-  // YOU'RE SCREWED
+  
 }
